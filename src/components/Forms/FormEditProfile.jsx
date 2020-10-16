f5d04df1fb708b380d05c52585510696949cd711
Lcdevi <caillauxlaura@gmail.com>
2020-10-16 15:46:25 +0200

    begin form edit profile

--- src/components/Forms/FormEditProfile.jsx
+++ src/components/Forms/FormEditProfile.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Form } from 'semantic-ui-react';
 
-const FormEditProfile = () => {
-    return (
-        <div>
-            <h1>FORM EDIT PROFIL</h1>
-        </div>
-    )
+class FormEditProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>FORM EDIT PROFIL</h1>
+                <form>
+                    <label htmlFor=""></label>
+                </form>
+            </div>
+        )
+    }
+
 }
 
 export default FormEditProfile
