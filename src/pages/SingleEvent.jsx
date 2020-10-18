c8183f8232d7936c1883997c5e542e19c6fadaa8
Lcdevi <caillauxlaura@gmail.com>
2020-10-18 18:58:27 +0200

    single event page with populate

--- src/pages/SingleEvent.jsx
+++ src/pages/SingleEvent.jsx
@@ -14,8 +14,10 @@ class SingleUser extends React.Component {
         apiHandler
             .getOne("/api/event/", this.props.match.params.eventId)
             .then((apiRes) => {
+                console.log("------------------",apiRes)
                 this.setState({ event: apiRes })
-                console.log(this.state)
+                // console.log(this.state)
+                // console.log(this.state.event.userId)
             })
             .catch((error) => {
                 console.log(error)
@@ -29,8 +31,18 @@ class SingleUser extends React.Component {
 
         return (
             <div id="main">
-                <h1>EVENT</h1>
-                <h2>HELLO{this.state.event.name}</h2>
+                <div id="singleUser-main">
+                    <div className="singleUser-container">
+                        <div>
+                            <h1>EVENT</h1>
+                            <h2>{this.state.event.name}</h2>
+                            <br />
+                            <p>Un événement créé par :  <br /> <span>{this.state.event.userId.firstName} {this.state.event.userId.lastName}</span></p>
+                        </div>
+                        <Image src={this.state.event.mainImageUrl} size='medium' rounded alt="profil picture" width="100px" />
+                    </div>
+                    <p><div className="description-singleUser"><i>A propos <br /> <span>{this.state.event.description}</span></i></div></p>
+                </div>
             </div>
         )
     }
