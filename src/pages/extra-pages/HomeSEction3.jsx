5a33a2bdc141f5bca241489b81f9a90fbaaaedbd
Rico <rasim.rico@gmail.com>
2020-10-23 11:55:15 +0200

    before deploying

--- src/pages/extra-pages/HomeSEction3.jsx
+++ src/pages/extra-pages/HomeSEction3.jsx
@@ -16,12 +16,12 @@ const HomeSEction3 = () => {
       <div data-aos="fade-up" data-aos-delay="1500" className="title">
         <Link style={{ color: "black" }} to="/HomeSection2">
           {" "}
-          <h1 className="headline">See some sercet place</h1>
+          <h1 className="headline">See some secret place</h1>
         </Link>
       </div>
 
       <div data-aos="fade-up" data-aos-delay="1900" className="title">
-        <h1 className="main-headline">cool activties</h1>
+        <h1 className="main-headline">cool activities</h1>
         <span className="subheadline">Discover</span>
       </div>
     </div>
